35ab36a708894d485fbd5557b60ba30a12271c6f
1DIOXIN1 <109106526+1DIOXIN1@users.noreply.github.com>
2024-10-19 00:45:11 +0500

    TestRep
    Тест репозитория

---
+++ Assets/Scripts/Test.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class Test : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Debug.Log("123123123");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
