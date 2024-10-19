4069e9156ce23333128fda041069796248150ae9
1DIOXIN1 <109106526+1DIOXIN1@users.noreply.github.com>
2024-10-19 15:58:14 +0500

    ShakeCamera
    Добавил тряску камеры при движении

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -1,11 +1,18 @@
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.SocialPlatforms;
 
 public class CameraController : MonoBehaviour
 {
     [SerializeField] private float _sensitivity = 2.0f; //Чувствительность мыши
     [SerializeField] private float _maxYAngle; //Макс угол вращения по вертикали
 
+    private ShakeCamera _cameraShake;
     private float _rotationX = 0.0f;
+    private void Start() 
+    {
+        _cameraShake = GetComponent<ShakeCamera>();
+    }
     private void Update() {
         //Получение ввода от мыши
         float mouseX = Input.GetAxis("Mouse X");
@@ -18,5 +25,12 @@ public class CameraController : MonoBehaviour
         _rotationX -= mouseY * _sensitivity;
         _rotationX = Mathf.Clamp(_rotationX, -_maxYAngle, _maxYAngle);
         transform.localRotation = Quaternion.Euler(_rotationX, 0.0f, 0.0f);
+    
+    float move = Input.GetAxis("Vertical");
+    if (Mathf.Abs(move) > 0) // Если персонаж двигается
+    {
+        _cameraShake.Shake();
     }
+    }
+
 }
