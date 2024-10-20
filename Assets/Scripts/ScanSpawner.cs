95e4248d85210c99d677a6d485cb2b53e51dfdc3
Lelushik <albert.muhamet123@gmail.com>
2024-10-20 12:33:26 +0500

    REAL Black Update

--- Assets/Scripts/ScanSpawner.cs
+++ Assets/Scripts/ScanSpawner.cs
@@ -1,15 +1,30 @@
+using UnityEditor.Rendering.Universal;
 using UnityEngine;
+using UnityEngine.Rendering.Universal;
 
 public class ScanSpawner : MonoBehaviour
 {
     [SerializeField] private GameObject echoWavePrefab;  // Префаб сферы эхолокатора
     [SerializeField] private Transform spawnPoint;       // Точка, где будет спавниться сфера
+    [SerializeField] private UniversalRendererData rendererData;
 
     void Update()
     {
         // Проверяем нажатие клавиши пробел (Space)
         if (Input.GetKeyDown(KeyCode.Space))
         {
+            if (rendererData != null)
+            {
+                // Ищем FullScreenPassRendererFeature среди rendererFeatures
+                for (int i = 0; i < rendererData.rendererFeatures.Count; i++)
+                {
+                    if (rendererData.rendererFeatures[i] is FullScreenPassRendererFeature)
+                    {
+                        // Отключаем FullScreenPassRendererFeature
+                        rendererData.rendererFeatures[i].SetActive(true);
+                    }
+                }
+            }
             SpawnEchoWave();
         }
     }
